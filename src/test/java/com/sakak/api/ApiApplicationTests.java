bece74ae5ae460322db74b7bffa181ca4a4f7458
jaeyeon kim <jakind@naver.com>
2024-05-26 03:10:43 +0900

    chore: 프로젝트 설정

---
+++ src/test/java/com/sakak/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakak.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
