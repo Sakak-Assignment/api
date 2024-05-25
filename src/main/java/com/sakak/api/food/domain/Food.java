35b20492a3a9b20ec20ab3b427b4eb472ab3ec1a
jaeyeon kim <jakind@naver.com>
2024-05-26 06:35:35 +0900

    refactor: Food -> Nutrient 간의 직접참조 제거

--- src/main/java/com/sakak/api/food/domain/Food.java
+++ src/main/java/com/sakak/api/food/domain/Food.java
@@ -4,15 +4,11 @@ import com.sakak.api.food.domain.vo.FoodCode;
 import com.sakak.api.food.domain.vo.FoodName;
 import com.sakak.api.food.domain.vo.GroupName;
 import com.sakak.api.food.domain.vo.MakerName;
-import com.sakak.api.nutrient.domain.Nutrient;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -37,9 +33,6 @@ public class Food {
 
     @Embedded
     private GroupName groupName;
-
-    @OneToMany(mappedBy = "food")
-    private final List<Nutrient> nutrients = new ArrayList<>();
 
     private Food(FoodCode foodCode, FoodName foodName, MakerName makerName, GroupName groupName) {
         this.foodCode = foodCode;
