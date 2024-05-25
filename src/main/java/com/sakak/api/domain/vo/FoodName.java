0e54a9856a81d9c496df2238691ecd907004515f
jaeyeon kim <jakind@naver.com>
2024-05-26 04:09:34 +0900

    feat: FoodName,MakerName VO 적용

---
+++ src/main/java/com/sakak/api/domain/vo/FoodName.java
@@ -0,0 +1,31 @@
+package com.sakak.api.domain.vo;
+
+import jakarta.persistence.Embeddable;
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class FoodName {
+
+    private String name;
+
+    private FoodName(String name) {
+        this.name = name;
+    }
+
+    public static FoodName from(String name) {
+        validateFoodName(name);
+        return new FoodName(name);
+    }
+
+    private static void validateFoodName(String foodName) {
+        if (Objects.isNull(foodName) || foodName.isEmpty()) {
+            throw new IllegalArgumentException("invalid food name");
+        }
+    }
+
+}
---
+++ src/main/java/com/sakak/api/domain/vo/MakerName.java
@@ -0,0 +1,31 @@
+package com.sakak.api.domain.vo;
+
+import jakarta.persistence.Embeddable;
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class MakerName {
+
+    private String name;
+
+    private MakerName(String name) {
+        this.name = name;
+    }
+
+    public static MakerName from(String name) {
+        validateMakerName(name);
+        return new MakerName(name);
+    }
+
+    private static void validateMakerName(String makerName) {
+        if (Objects.isNull(makerName) || makerName.isEmpty()) {
+            throw new IllegalArgumentException("invalid maker name");
+        }
+    }
+
+}
