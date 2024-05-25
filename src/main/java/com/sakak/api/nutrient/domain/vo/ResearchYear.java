658650f9d8aacf530c3135bec45c1d642f3f90f8
jaeyeon kim <jakind@naver.com>
2024-05-26 05:11:00 +0900

    refactor: primitive 타입으로 변경

--- src/main/java/com/sakak/api/nutrient/domain/vo/ResearchYear.java
+++ src/main/java/com/sakak/api/nutrient/domain/vo/ResearchYear.java
@@ -3,7 +3,6 @@ package com.sakak.api.nutrient.domain.vo;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.time.LocalDateTime;
-import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,25 +12,22 @@ import lombok.NoArgsConstructor;
 @Getter
 public class ResearchYear {
 
-    private static final Long MIN_RESEARCH_YEAR = 1900L;
+    private static final long MIN_RESEARCH_YEAR = 1900L;
 
     @Column(nullable = false)
-    private Long year;
+    private long year;
 
-    private ResearchYear(Long year) {
+    private ResearchYear(long year) {
         this.year = year;
     }
 
-    public static ResearchYear from(Long year) {
+    public static ResearchYear from(long year) {
         validateResearchYear(year);
         return new ResearchYear(year);
     }
 
-    private static void validateResearchYear(Long year) {
-        if (Objects.isNull(year)
-            || year < MIN_RESEARCH_YEAR
-            && LocalDateTime.now().getYear() < year
-        ) {
+    private static void validateResearchYear(long year) {
+        if (year < MIN_RESEARCH_YEAR && LocalDateTime.now().getYear() < year) {
             throw new IllegalArgumentException("invalid research year");
         }
     }
--- src/main/java/com/sakak/api/nutrient/domain/vo/ServingSize.java
+++ src/main/java/com/sakak/api/nutrient/domain/vo/ServingSize.java
@@ -2,7 +2,6 @@ package com.sakak.api.nutrient.domain.vo;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,19 +12,19 @@ import lombok.NoArgsConstructor;
 public class ServingSize {
 
     @Column(nullable = false)
-    private Long size;
+    private long size;
 
-    private ServingSize(Long size) {
+    private ServingSize(long size) {
         this.size = size;
     }
 
-    public static ServingSize from(Long size) {
+    public static ServingSize from(long size) {
         validateServingSize(size);
         return new ServingSize(size);
     }
 
-    private static void validateServingSize(Long servingSize) {
-        if (Objects.isNull(servingSize) || servingSize <= 0) {
+    private static void validateServingSize(long servingSize) {
+        if (servingSize <= 0) {
             throw new IllegalArgumentException("invalid serving size");
         }
     }
